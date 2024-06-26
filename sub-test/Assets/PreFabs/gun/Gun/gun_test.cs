8d2662202c4831bdf67cf0b85ce11d8f483bde33
Niek Middel <niek.middel@stichtingheldeninit.nl>
2024-06-26 15:57:09 +0200

    finishing up for the day

--- sub-test/Assets/PreFabs/gun/Gun/gun_test.cs
+++ sub-test/Assets/PreFabs/gun/Gun/gun_test.cs
@@ -11,31 +11,23 @@ public class gun_test : MonoBehaviour
     [SerializeField] private float HoldCords_y;
     [SerializeField] private float HoldCords_z;
     [SerializeField] private Animator animator;
-    [SerializeField] private Animator arms;
-    [SerializeField] public Transform am;
+    [SerializeField] private Animator arms; 
+    [SerializeField] private string includeArms = "_gun-arm";
+    [SerializeField] private String[] animationNames;
     private bool shooting;
     private bool reloading;
     // Start is called before the first frame update
     void Start()
     {
         animator.Play("pose");           
-        am = transform.parent.parent.Find("arms3");
-        Debug.Log(am);
-        try 
-        { 
-            arms = am.GetComponent<Animator>();
-        } catch (Exception e)
-        {
-            Debug.Log($"sucks to  be you {e}");
-        }
         
-        Debug.Log(arms);
+        arms = transform.parent.parent.Find("arms3").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = new Vector3(HoldCords_x,HoldCords_y,HoldCords_z);
+        //transform.localPosition = new Vector3(HoldCords_x,HoldCords_y,HoldCords_z);
 
     }
 
@@ -46,25 +38,28 @@ public class gun_test : MonoBehaviour
         Debug.Log("hewo");
         Invoke("animeReset",.5f);
         shooting = true;
-        animator.Play("fire");
+        animator.Play(animationNames[0]);
+        arms.Play(animationNames[0] + includeArms);
         Debug.Log("");
        }
     }
     private void OnMove()
     {
-
+        
     }
     private void animeReset()
     {
         shooting = false;
         reloading = false;
         animator.Play("pose");
+        arms.Play("pose" + includeArms);
     }
     private void OnReload()
     {
         reloading = true;
         Invoke("animeReset", 2.5f);
         animator.Play("reload");
+        arms.Play("reload" + includeArms);
     }
     /*
     private IEnumerator Pplay()
